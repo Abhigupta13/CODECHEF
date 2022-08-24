8f625ecab29b58b97cfd404190acd4a8dd485d9f
Abhishek Kumar Gupta <100848632+abhishekgupta-akg07@users.noreply.github.com>
2022-08-24 18:33:28 +0530

    Update acs.cpp

--- acs.cpp
+++ acs.cpp
@@ -4,7 +4,7 @@ There are 10 problems in a contest. You know that the score of each problem is e
 
 Chef came to know the total score of a participant and he is wondering how many problems were actually solved by that participant.
 
-Given the total score PP of the participant, determine the number of problems solved by the participant. Print -1 in case the score is invalid.
+Given the total score P of the participant, determine the number of problems solved by the participant. Print -1 in case the score is invalid.
   
   
   
